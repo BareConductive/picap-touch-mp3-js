56fa42f66b1f92fe4d42709fc7f82a7f3da3f517
Pascal Loose <pascal.loose@me.com>
2017-06-07 15:01:10 +0100

    Added threshold values

--- touch-mp3.js
+++ touch-mp3.js
@@ -6,11 +6,11 @@
   touch-mp3.js - monophonic touch triggered MP3 playback
 
   ****************************** IMPORTANT NOTE ********************************
-  
-  This code plays back files monophonically, unlike our other Pi Cap MP3 
+
+  This code plays back files monophonically, unlike our other Pi Cap MP3
   examples, which are polyphonic. This is due to restrictictions in availble
-  Node.js libraries and techniques. If you want polyphonic playback, please 
-  choose from an example written in another language. If you think you can do 
+  Node.js libraries and techniques. If you want polyphonic playback, please
+  choose from an example written in another language. If you think you can do
   polyphonic MP3 playback in Node.js, please let us know how!
 
   ******************************************************************************
@@ -20,19 +20,19 @@
   Bare Conductive code written by Szymon Kaliski.
 
   This work is licensed under a MIT license https://opensource.org/licenses/MIT
-  
+
   Copyright (c) 2016, Bare Conductive
-  
+
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
   in the Software without restriction, including without limitation the rights
   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
   copies of the Software, and to permit persons to whom the Software is
   furnished to do so, subject to the following conditions:
-  
+
   The above copyright notice and this permission notice shall be included in all
   copies or substantial portions of the Software.
-  
+
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -51,8 +51,19 @@ var execSync = require('child_process').execSync;
 var MPR121 = require('node-picap');
 var mpr121;
 
+// this is the touch threshold - setting it low makes it more like a proximity trigger
+// default value is 40 for touch
+var touchThreshold = 40;
+
+// this is the release threshold - must ALWAYS be smaller than the touch threshold
+// default value is 20 for touch
+var releaseThreshold = 20;
+
 // correct address for the Pi Cap - other boards may vary
-mpr121 = new MPR121('0x5C'); 
+mpr121 = new MPR121('0x5C');
+
+mpr121.setTouchThreshold(touchThreshold);
+mpr121.setReleaseThreshold(releaseThreshold);
 
 // set up LED
 var gpioRed   = new Gpio(6,  'out', 'none', { activeLow: true });
